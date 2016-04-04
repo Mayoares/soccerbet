abb90ac368361cbf16ec201ef76eebb951ec7e07
Mayoares <andymsdn@gmx.de>
2016-04-04 22:16:20 +0200

    Teams

---
+++ Tippspiel/EM2016/dbimport/Teams2016.sql
@@ -0,0 +1,29 @@
+INSERT INTO `emtipp2016`.`emtipp2016_teams` (`name`, `shortname`, `logofile`, `group`) VALUES ('Frankreich', 'FRA', 'flagge-frankreich-flagge-rechteckigschwarz-15x23.gif', 'A');
+INSERT INTO `emtipp2016`.`emtipp2016_teams` (`name`, `shortname`, `logofile`, `group`) VALUES ('Schweiz', 'SUI', '20px-Flag_of_Switzerland_within_2to3.svg.png', 'A');
+INSERT INTO `emtipp2016`.`emtipp2016_teams` (`name`, `shortname`, `logofile`, `group`) VALUES ('Rumänien', 'ROM', 'rumaenien.png', 'A');
+INSERT INTO `emtipp2016`.`emtipp2016_teams` (`name`, `shortname`, `logofile`, `group`) VALUES ('Albanien', 'ALB', 'albanien.png', 'A');
+
+INSERT INTO `emtipp2016`.`emtipp2016_teams` (`name`, `shortname`, `logofile`, `group`) VALUES ('England', 'ENG', 'flagge-england-flagge-rechteckigschwarz-15x25.gif', 'B');
+INSERT INTO `emtipp2016`.`emtipp2016_teams` (`name`, `shortname`, `logofile`, `group`) VALUES ('Russland', 'RUS', 'Flag_of_Russia.svg.png', 'B');
+INSERT INTO `emtipp2016`.`emtipp2016_teams` (`name`, `shortname`, `logofile`, `group`) VALUES ('Wales', 'WAL', 'wales.png', 'B');
+INSERT INTO `emtipp2016`.`emtipp2016_teams` (`name`, `shortname`, `logofile`, `group`) VALUES ('Slowakei', 'SVK', 'slowakei.png', 'B');
+
+INSERT INTO `emtipp2016`.`emtipp2016_teams` (`name`, `shortname`, `logofile`, `group`) VALUES ('Deutschland', 'GER', 'flagge-deutschland-flagge-rechteckigschwarz-15x25.gif', 'C');
+INSERT INTO `emtipp2016`.`emtipp2016_teams` (`name`, `shortname`, `logofile`, `group`) VALUES ('Ukraine', 'UKR', 'flagge-ukraine-flagge-rechteckigschwarz-15x23.gif', 'C');
+INSERT INTO `emtipp2016`.`emtipp2016_teams` (`name`, `shortname`, `logofile`, `group`) VALUES ('Polen', 'POL', 'flagge-polen-flagge-rechteckigschwarz-15x24.gif', 'C');
+INSERT INTO `emtipp2016`.`emtipp2016_teams` (`name`, `shortname`, `logofile`, `group`) VALUES ('Nordirland', 'NIR', 'nordirland.png', 'C');
+
+INSERT INTO `emtipp2016`.`emtipp2016_teams` (`name`, `shortname`, `logofile`, `group`) VALUES ('Spanien', 'ESP', 'flagge-spanien-flagge-rechteckigschwarz-15x22.gif', 'D');
+INSERT INTO `emtipp2016`.`emtipp2016_teams` (`name`, `shortname`, `logofile`, `group`) VALUES ('Tschechien', 'CZE', 'flagge-tschechische-republik-flagge-rechteckigschwarz-15x23.gif', 'D');
+INSERT INTO `emtipp2016`.`emtipp2016_teams` (`name`, `shortname`, `logofile`, `group`) VALUES ('Türkei', 'TUR', 'tuerkei.png', 'D');
+INSERT INTO `emtipp2016`.`emtipp2016_teams` (`name`, `shortname`, `logofile`, `group`) VALUES ('Kroatien', 'CRO', 'Flag_of_Croatia.svg.png', 'D');
+
+INSERT INTO `emtipp2016`.`emtipp2016_teams` (`name`, `shortname`, `logofile`, `group`) VALUES ('Belgien', 'BEL', '20px-Flag_of_Belgium_(civil).svg.png', 'E');
+INSERT INTO `emtipp2016`.`emtipp2016_teams` (`name`, `shortname`, `logofile`, `group`) VALUES ('Italien', 'ITA', 'flagge-italien-flagge-rechteckigschwarz-15x26.gif', 'E');
+INSERT INTO `emtipp2016`.`emtipp2016_teams` (`name`, `shortname`, `logofile`, `group`) VALUES ('Irland', 'IRL', 'irland.png', 'E');
+INSERT INTO `emtipp2016`.`emtipp2016_teams` (`name`, `shortname`, `logofile`, `group`) VALUES ('Schweden', 'SWE', 'schweden.png', 'E');
+
+INSERT INTO `emtipp2016`.`emtipp2016_teams` (`name`, `shortname`, `logofile`, `group`) VALUES ('Portugal', 'POR', 'flagge-portugal-flagge-rechteckigschwarz-15x23.gif', 'F');
+INSERT INTO `emtipp2016`.`emtipp2016_teams` (`name`, `shortname`, `logofile`, `group`) VALUES ('Island', 'ISL', 'island.png', 'F');
+INSERT INTO `emtipp2016`.`emtipp2016_teams` (`name`, `shortname`, `logofile`, `group`) VALUES ('Österreich', 'AUT', 'oesterreich.png', 'F');
+INSERT INTO `emtipp2016`.`emtipp2016_teams` (`name`, `shortname`, `logofile`, `group`) VALUES ('Ungarn', 'HUN', 'ungarn.png', 'F');
