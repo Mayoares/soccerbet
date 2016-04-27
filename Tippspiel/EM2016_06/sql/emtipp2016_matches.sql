3a9d2b1ec379edab09bbfd56c393e07fa8dca79c
imperator-soccerbet <robertkalusok@192.168.2.138>
2016-04-27 12:26:17 +0200

    Fehler in emtipp2016_matches.sql behoben.

--- Tippspiel/EM2016_06/sql/emtipp2016_matches.sql
+++ Tippspiel/EM2016_06/sql/emtipp2016_matches.sql
@@ -1,36 +1,104 @@
 -- --------------------------------------------------------
 
 --
--- Daten für Tabelle `emtipp2016_teams`
+-- Daten für Tabelle `emtipp2016_matches`
 --
 
-INSERT INTO `emtipp2016`.`emtipp2016_teams` (`name`, `shortname`, `logofile`, `group`) VALUES
-('Frankreich', 'FRA', 'flagge_frankreich.jpg', 'A'),
-('Schweiz', 'SUI', 'flagge_schweiz.jpg', 'A'),
-('Rumänien', 'ROM', 'flagge_rumaenien.jpg', 'A'),
-('Albanien', 'ALB', 'flagge_albanien.jpg', 'A'),
+INSERT INTO `emtipp2016`.`emtipp2016_matches` (`matchnr`, `group`, `team1`, `team2`, `matchdate`, `matchtime`, `matchtype`, `evaluationDone`) VALUES
+-- --------------------------------------------------------
+-- SPIELTAG 1 GRUPPENSPIEL
+-- --------------------------------------------------------
+(1, 'A', 'FRA', 'ROM', '10.6.', '21:00:00', 'Gruppenspiel', 'F'),
 --
-('England', 'ENG', 'flagge_england.jpg', 'B'),
-('Russland', 'RUS', 'flagge_russland.jpg', 'B'),
-('Wales', 'WAL', 'flagge_wales.jpg', 'B'),
-('Slowakei', 'SVK', 'flagge_slowakei.jpg', 'B'),
+(2, 'A', 'ALB', 'SUI', '11.6.', '15:00:00', 'Gruppenspiel', 'F'),
+(3, 'B', 'WAL', 'SVK', '11.6.', '18:00:00', 'Gruppenspiel', 'F'),
+(4, 'B', 'ENG', 'RUS', '11.6.', '21:00:00', 'Gruppenspiel', 'F'),
 --
-('Deutschland', 'GER', 'flagge_deutschland.jpg', 'C'),
-('Ukraine', 'UKR', 'flagge_ukraine.jpg', 'C'),
-('Polen', 'POL', 'flagge_polen.jpg', 'C'),
-('Nordirland', 'NIR', 'flagge_nordirland.jpg', 'C'),
+(5, 'D', 'TUR', 'CRO', '12.6.', '15:00:00', 'Gruppenspiel', 'F'),
+(6, 'C', 'POL', 'NIR', '12.6.', '18:00:00', 'Gruppenspiel', 'F'),
+(7, 'C', 'GER', 'UKR', '12.6.', '21:00:00', 'Gruppenspiel', 'F'),
 --
-('Spanien', 'ESP', 'flagge_spanien.jpg', 'D'),
-('Tschechien', 'CZE', 'flagge_tschechien.jpg', 'D'),
-('Türkei', 'TUR', 'flagge_tuerkei.jpg', 'D'),
-('Kroatien', 'CRO', 'flagge_kroatien.jpg', 'D'),
+(8, 'D', 'ESP', 'CZE', '13.6.', '15:00:00', 'Gruppenspiel', 'F'),
+(9, 'E', 'IRL', 'SWE', '13.6.', '18:00:00', 'Gruppenspiel', 'F'),
+(10, 'E', 'BEL', 'ITA', '13.6.', '21:00:00', 'Gruppenspiel', 'F'),
 --
-('Belgien', 'BEL', 'flagge_belgien.jpg', 'E'),
-('Italien', 'ITA', 'flagge_italien.jpg', 'E'),
-('Irland', 'IRL', 'flagge_irland.jpg', 'E'),
-('Schweden', 'SWE', 'flagge_schweden.jpg', 'E'),
+(11, 'F', 'AUT', 'HUN', '14.6.', '18:00:00', 'Gruppenspiel', 'F'),
+(12, 'F', 'POR', 'ISL', '14.6.', '21:00:00', 'Gruppenspiel', 'F'),
+-- --------------------------------------------------------
+-- SPIELTAG 2 GRUPPENSPIEL
+-- --------------------------------------------------------
+(13, 'B', 'RUS', 'SVK', '15.6.', '15:00:00', 'Gruppenspiel', 'F'),
+(14, 'A', 'ROM', 'SUI', '15.6.', '18:00:00', 'Gruppenspiel', 'F'),
+(15, 'A', 'FRA', 'ALB', '15.6.', '21:00:00', 'Gruppenspiel', 'F'),
 --
-('Portugal', 'POR', 'flagge_portugal.jpg', 'F'),
-('Island', 'ISL', 'flagge_island.jpg', 'F'),
-('Österreich', 'AUT', 'flagge_oesterreich.jpg', 'F'),
-('Ungarn', 'HUN', 'flagge_ungarn.jpg', 'F');
+(16, 'B', 'ENG', 'WAL', '16.6.', '15:00:00', 'Gruppenspiel', 'F'),
+(17, 'C', 'UKR', 'NIR', '16.6.', '18:00:00', 'Gruppenspiel', 'F'),
+(18, 'C', 'GER', 'POL', '16.6.', '21:00:00', 'Gruppenspiel', 'F'),
+--
+(19, 'E', 'ITA', 'SWE', '17.6.', '15:00:00', 'Gruppenspiel', 'F'),
+(20, 'D', 'CZE', 'CRO', '17.6.', '18:00:00', 'Gruppenspiel', 'F'),
+(21, 'D', 'ESP', 'TUR', '17.6.', '21:00:00', 'Gruppenspiel', 'F'),
+--
+(22, 'E', 'BEL', 'IRL', '18.6.', '15:00:00', 'Gruppenspiel', 'F'),
+(23, 'F', 'ISL', 'HUN', '18.6.', '18:00:00', 'Gruppenspiel', 'F'),
+(24, 'F', 'POR', 'AUT', '18.6.', '21:00:00', 'Gruppenspiel', 'F'),
+-- --------------------------------------------------------
+-- SPIELTAG 3 GRUPPENSPIEL
+-- --------------------------------------------------------
+(25, 'A', 'SUI', 'FRA', '19.6.', '21:00:00', 'Gruppenspiel', 'F'),
+(26, 'A', 'ROM', 'ALB', '19.6.', '21:00:00', 'Gruppenspiel', 'F'),
+--
+(27, 'B', 'SVK', 'ENG', '20.6.', '21:00:00', 'Gruppenspiel', 'F'),
+(28, 'B', 'RUS', 'WAL', '20.6.', '21:00:00', 'Gruppenspiel', 'F'),
+--
+(29, 'C', 'NIR', 'GER', '21.6.', '18:00:00', 'Gruppenspiel', 'F'),
+(30, 'C', 'UKR', 'POL', '21.6.', '18:00:00', 'Gruppenspiel', 'F'),
+(31, 'D', 'CRO', 'ESP', '21.6.', '21:00:00', 'Gruppenspiel', 'F'),
+(32, 'D', 'CZE', 'TUR', '21.6.', '21:00:00', 'Gruppenspiel', 'F'),
+--
+(33, 'F', 'ISL', 'AUT', '22.6.', '18:00:00', 'Gruppenspiel', 'F'),
+(34, 'F', 'HUN', 'POR', '22.6.', '18:00:00', 'Gruppenspiel', 'F'),
+(35, 'E', 'SWE', 'BEL', '22.6.', '21:00:00', 'Gruppenspiel', 'F'),
+(36, 'E', 'ITA', 'IRL', '22.6.', '21:00:00', 'Gruppenspiel', 'F');
+--
+-- (37, 'C', 'JPN', 'COL', '24.6.', '22:00:00', 'Gruppenspiel', 'F'),
+-- (38, 'C', 'GRE', 'CIV', '24.6.', '22:00:00', 'Gruppenspiel', 'F'),
+-- (39, 'D', 'ITA', 'URU', '24.6.', '18:00:00', 'Gruppenspiel', 'F'),
+-- (40, 'D', 'CRC', 'ENG', '24.6.', '18:00:00', 'Gruppenspiel', 'F'),
+-- (41, 'E', 'HON', 'SUI', '25.6.', '22:00:00', 'Gruppenspiel', 'F'),
+-- (42, 'E', 'ECU', 'FRA', '25.6.', '22:00:00', 'Gruppenspiel', 'F'),
+-- (43, 'F', 'NGA', 'ARG', '25.6.', '18:00:00', 'Gruppenspiel', 'F'),
+-- (44, 'F', 'BIH', 'IRN', '25.6.', '18:00:00', 'Gruppenspiel', 'F'),
+-- (45, 'G', 'USA', 'GER', '26.6.', '18:00:00', 'Gruppenspiel', 'F'),
+-- (46, 'G', 'POR', 'GHA', '26.6.', '18:00:00', 'Gruppenspiel', 'F'),
+-- (47, 'H', 'KOR', 'BEL', '26.6.', '22:00:00', 'Gruppenspiel', 'F'),
+-- (48, 'H', 'ALG', 'RUS', '26.6.', '22:00:00', 'Gruppenspiel', 'F');
+
+INSERT INTO `emtipp2016`.`emtipp2016_matches` (`matchnr`, `group`, `team1`, `team2`, `matchdate`, `matchtime`, `matchtype`, `evaluationDone`) VALUES
+(37, '', '', '', '25.6.', '15:00:00', 'Achtelfinale', 'F'),
+(38, '', '', '', '25.6.', '18:00:00', 'Achtelfinale', 'F'),
+(39, '', '', '', '25.6.', '21:00:00', 'Achtelfinale', 'F'),
+(40, '', '', '', '26.6.', '15:00:00', 'Achtelfinale', 'F'),
+(41, '', '', '', '26.6.', '18:00:00', 'Achtelfinale', 'F'),
+(42, '', '', '', '26.6.', '21:00:00', 'Achtelfinale', 'F'),
+(43, '', '', '', '27.6.', '18:00:00', 'Achtelfinale', 'F'),
+(44, '', '', '', '27.6.', '21:00:00', 'Achtelfinale', 'F'),
+--
+(45, '', '', '', '30.6', '21:00:00', 'Viertelfinale', 'F'),
+(46, '', '', '', '1.7.', '21:00:00', 'Viertelfinale', 'F'),
+(47, '', '', '', '2.7.', '21:00:00', 'Viertelfinale', 'F'),
+(48, '', '', '', '3.7.', '21:00:00', 'Viertelfinale', 'F'),
+--
+(49, '', '', '', '6.7.', '21:00:00', 'Halbfinale', 'F'),
+(50, '', '', '', '7.7.', '21:00:00', 'Halbfinale', 'F'),
+--
+-- (, '', '', '', '', '', 'Platz3', 'F'),
+--
+(51, '', '', '', '10.7.', '21:00:00', 'Finale', 'F');
+
+
+
+
+
+
+
