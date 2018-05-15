1986e1bd5cef542eefe8f7495be250b2479ae800
Mayoares <andreas.grotemeyer@gmail.com>
2018-05-15 22:00:02 +0200

    11 Zitate von Werke eingetragen --> Viertel-, Halb- und Finale haben noch die Zitate vom letzten Mal.

--- Tippspiel/WM2018/sql/INSERT INTO/wmtipp2018_groupcitations.sql
+++ Tippspiel/WM2018/sql/INSERT INTO/wmtipp2018_groupcitations.sql
@@ -1,15 +1,17 @@
 INSERT INTO `wmtipp2018_groupcitations` (`group`, `citation`, `author`) VALUES
-('A', 'Mir ist es egal, ob es ein Brasilianer, Pole, Kroate, Norddeutscher oder Süddeutscher ist. Die Leistung entscheidet, nicht irgendeine Blutgruppe.\r\n', 'Christoph Daum'),
-('B', 'Wir müssen gewinnen, alles andere ist primär.\r\n', 'Hans Krankl'),
-('C', 'Wir müssen jetzt die Köpfe hochkrempeln – und die Ärmel auch.\r\n', 'Lukas Podolski'),
-('D', 'Der Rasen sieht alt und gebraucht aus, irgendwie erinnert er mich an die Kleider der Kelly Family.\r\n', 'Beni Thurnheer, Schweizer Sportreporter'),
-('E', 'Die Schweden sind keine Holländer, das hat man ganz klar gesehen!\r\n', 'Franz Beckenbauer'),
-('F', 'Langsam habe ich das Gefühl, dass ich mit meinem linken Fuß mehr anfangen kann, als nur Bier zu holen.\r\n', 'Thomas Müller'),
+('A', 'Auch größenmäßig ist es der größte Nachteil, dass die Torhüter in Japan nicht die Allergrößten sind!\r\n', 'Klaus Lufen'),
+('B', 'Das hätte in der Türkei passieren dürfen, aber nicht in der zivilisierten Welt.\r\n', 'Toni Schumacher'),
+('C', 'Die Brasilianer sind ja auch alle technisch serviert.\r\n', 'Andreas Brehme'),
+('D', 'Die Kroaten sollen ja auf alles treten, was sich bewegt - da hat unser Mittelfeld ja nichts zu befürchten.\r\n', 'Berti Vogts'),
+('E', 'Die Schotten sind meistens eher zu Hause als ihre Postkarten.\r\n', 'Wilfried Mohren'),
+('F', 'Die Schweden sind keine Holländer - das hat man ganz klar gesehen.\r\n', 'Franz Beckenbauer'),
+('G', 'Das Tor gehört zu 70 % mir und zu 40 % dem Wilmots.\r\n', 'Ingo Anderbrügge'),
+('H', 'Das war europäische Weltklasse!\r\n', 'Felix Magath'),
 --
-('Tor', 'Ja, Statistiken. Aber welche Statistik stimmt schon? Nach der Statistik ist jeder vierte Mensch ein Chinese, aber hier spielt gar kein Chinese mit.\r\n', 'Werner Hansch'),
-('Stockerl', 'Ich hatte vom Feeling her ein gutes Gefühl.\r\n', 'Andi Möller'),
+('Tor', 'Es ist wichtig, dass man sich neunzig Minuten mit voller Konzentration an das nächste Spiel denkt!\r\n', 'Lothar Matthäus'),
+('Stockerl', 'Fußball ist inzwischen die Nr. 1 in Frankreich. Handball übrigens auch!\r\n', 'Heribert Fassbender'),
 --
-('Achtelfinale', 'Brasilianer waren sie noch nie, die Griechen (Der ORF-Kommentator beim Spiel Griechenland-Russland bei der EM 2008)\r\n', 'Thomas König'),
+('Achtelfinale', 'Sie sollen nicht glauben, das sie Brasilianer sind, nur weil sie aus Brasilien kommen!\r\n', 'Franz Breitner'),
 ('Viertelfinale', 'Wir sehen uns auf der Piste wieder (nach dem 0:1 gegen Deutschland bei der EM 2008)\r\n', 'Österreichischer Fan'),
 ('Halbfinale', 'Jetzt fährt Boris Becker schon im Bus mit, bald bringen die Spieler ihre Tanten mit (über die Mitfahrer im Bus der deutschen Nationalmannschaft bei der EM 2000)\r\n', 'Eduard Geyer'),
 ('Finale', 'Eng (auf die Frage, wie es war, als Bundeskanzler Kohl nach dem EM-Sieg 1996 in die Kabine kam)\r\n', 'Mehmet Scholl');
